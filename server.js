5e9581eb9d9eddc3e350ae0093e14fbfd6283874
Programmer 5.1 0 <abiduzzamanrahat5@gmail.com>
2025-08-08 16:13:54 +0600

    Update server.js

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/submit-email', async (req, res) => {
   } catch (error) {
     // যদি শীটের নাম ডুপ্লিকেট হয়
     if (error.errors && error.errors[0].reason === 'duplicate') {
-      return res.status(200).json({ error: 'Sheet already exists for this email' });
+      return res.status(200).json({ message: 'Email already exists, proceed.' });
     }
     console.error(error);
     res.status(200).json({ error: 'Failed to save email' });
